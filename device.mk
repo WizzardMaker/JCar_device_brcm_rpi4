b15ed88516de01a0a6c32f04114e7aa6542b5170
Damian Petrecki <damian.petrecki@gmail.com>
2023-03-15 08:00:50 +0100

    Merge branch 'raspberry-vanilla:android-13.0' into android-13.0

--- device.mk
+++ device.mk
@@ -14,7 +14,7 @@ $(call inherit-product, vendor/brcm/rpi4/rpi4-vendor.mk)
 
 PRODUCT_AAPT_CONFIG := normal mdpi hdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi
-PRODUCT_CHARACTERISTICS := tablet,nosdcard
+PRODUCT_CHARACTERISTICS := automotive,nosdcard
 
 # API level
 PRODUCT_SHIPPING_API_LEVEL := 33
@@ -205,14 +205,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_tv.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_tv.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
-# Overlays
-PRODUCT_PACKAGES += \
-    AndroidRpiOverlay \
-    SettingsRpiOverlay \
-    SettingsProviderRpiOverlay \
-    SystemUIRpiOverlay \
-    WifiRpiOverlay
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/tablet_core_hardware.xml
@@ -234,7 +226,7 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/seccomp_policy/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
     $(DEVICE_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(DEVICE_PATH)/seccomp_policy/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy
-
+    
 # Storage
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
